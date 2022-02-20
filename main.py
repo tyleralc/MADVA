a285517f664daf5f909cf8cc6ceb146032aa5a3d
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-02-19 16:47:36 -0800

    ts

--- main.py
+++ main.py
@@ -30,16 +30,16 @@ professors =st.beta_container
 functions = st.beta_container
 motiv = st.beta_container
 
-with header: 
-    st.title('MADVA')
-    st.header('Master\'s in Applied Data Science Vitual Advisor')
-    st.subheader('By Tyler Alcorn and Chaimi Lee')
-    st.write("This app that helps USC students who are pursuing a Master's degree in Applied Data Science schedule and register for DSCI courses based on their interests and degree progress.")
+# with header:
+#     st.title('MADVA')
+#     st.header('Master\'s in Applied Data Science Vitual Advisor')
+#     st.subheader('By Tyler Alcorn and Chaimi Lee')
+#     st.write("This app that helps USC students who are pursuing a Master's degree in Applied Data Science schedule and register for DSCI courses based on their interests and degree progress.")
     
 
 
 with usc_courses:
-    st.title('DSCI Courses')
+    st.header('DSCI Courses')
     #insert a table with all the coureses laid out
     #include the professors who are 
     #example with an old dataframe
@@ -55,5 +55,5 @@ with functions:
     st.header("Functions")
 
 with motiv:
-    st.title("Motivations")
+    st.header("Motivations")
     st.write("Our motivation for creating MADVA (Master's in Applied Data science Virtual Advisor) was to help our fellow Applied Data Science students avoid these obstacles and better prepare ourselves for the coming semester and achieve our career goals. This app would significantly reduce stress during registration periods and help students plan future courses based on their interests and other students' experiences. This app would also be helpful for students who have less flexible schedules due to jobs, being in a different time zone, etc. With MADVA, our goal is to ensure that current and future DSCI students make the best out of their educational journey at USC.")
