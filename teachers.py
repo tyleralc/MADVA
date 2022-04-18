cbb25f55327962c86b47cf1a1c3e019b94cab374
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-17 17:20:16 -0700

    Update teachers.py

--- teachers.py
+++ teachers.py
@@ -1,19 +1,19 @@
 import requests 
 import pandas as pd
 from bs4 import BeautifulSoup
-
+import re
 
 
 def main():
     page= requests.get('https://catalogue.usc.edu/content.php?catoid=14&navoid=5199')
     soup = BeautifulSoup(page.content, 'html.parser')
-    #fac=soup.find(class_="block_content .table_default").get_text()
-    fac = soup.select(".block_content .table_content")
-    periods = [f.get_text() for f in fac]
-    # for f in fac:
-    #     f.get_text()
-    print(periods)
+    tbody= soup.tbody
+    for i in tbody:
+        print(i.get_text())
 
+    # text = [f.get_text() for f in p_tags]
+    
+  
 
 
 if __name__ == '__main__':
