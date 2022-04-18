c5adaedfa4d89e51b8f53168cd46e823395e8d84
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-18 12:10:44 -0700

    update

--- boto.py
+++ boto.py
@@ -1,33 +1,52 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
-
-table = dynamodb.create_table (
-    TableName = 'Employees',
-       KeySchema = [
-           {
-               'AttributeName': 'Name',
-               'KeyType': 'HASH'
-           },
-           {
-               'AttributeName': 'Email',
-               'KeyType': 'RANGE'
-           }
-           ],
-           AttributeDefinitions = [
-               {
-                   'AttributeName': 'Name',
-                   'AttributeType': 'S'
-               },
-               {
-                   'AttributeName':'Email',
-                   'AttributeType': 'S'
-               }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits':1,
-                'WriteCapacityUnits':1
-            }
-          
+client = boto3.client(
+    'dynamodb',
+    region_name= 'us-west-1',
+    aws_access_key_id='AKIA6EBMFI674ZVBGZW7',
+    aws_secret_access_key='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',
     )
-print(table)
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name= 'us-west-1',
+    aws_access_key_id='AKIA6EBMFI674ZVBGZW7',
+    aws_secret_access_key='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',
+    )
+ddb_exceptions = client.exceptions
+
+try:
+    table = client.create_table(
+        TableName='MADVA',
+        KeySchema=[
+            {
+                'AttributeName': 'PK',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'SK',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'PK',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'SK',
+                'AttributeType': 'S'
+            }
+    
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 40,
+            'WriteCapacityUnits': 20
+        }
+    )
+    print("Creating table")
+    waiter = client.get_waiter('table_exists')
+    waiter.wait(TableName='MADVA')
+    print("Table created")
+    
+except ddb_exceptions.ResourceInUseException:
+    print("Table exists")
