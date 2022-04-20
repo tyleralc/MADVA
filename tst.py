25cc88cff2c821e938259315c4359f7b9e9ec08d
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-19 22:10:37 -0700

    update tst

--- tst.py
+++ tst.py
@@ -4,3 +4,22 @@ import json
 response = requests.get('https://project-practice-8f9b2-default-rtdb.firebaseio.com/.json?orderBy="$key"')
 churned_dict=response.json()
 print(churned_dict)
+
+
+
+
+def get_email(course_name):
+
+
+    #get all the names of all_courses line 325 
+
+
+    f'{}@usc.edu'
+
+
+
+
+    # get studets who have similiar interests to you 
+
+
+    
